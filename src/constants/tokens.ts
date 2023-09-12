c658260710097120390088681fe3a06c3e4e543b
Horace Kuan <horacekuan@gmail.com>
2023-09-12 21:10:15 +1000

    Add font weight and text decoration to typography tokens

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -1,22 +1,32 @@
 export const typography = {
   headingLarge: {
     fontSize: '64px',
+    fontWeight: 700,
     lineHeight: '77px',
+    textDecoration: 'none',
   },
   headingMedium: {
     fontSize: '24px',
+    fontWeight: 400,
     lineHeight: '29px',
+    textDecoration: 'none',
   },
   headingSmall: {
     fontSize: '20px',
+    fontWeight: 400,
     lineHeight: '24px',
+    textDecoration: 'none',
   },
   bodyMedium: {
     fontSize: '18px',
+    fontWeight: 400,
     lineHeight: '24px',
+    textDecoration: 'none',
   },
   bodySmall: {
     fontSize: '14px',
+    fontWeight: 400,
     lineHeight: '17px',
+    textDecoration: 'underline',
   },
 };
