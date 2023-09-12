d54abd3a203251b6b4d3fed5f65331938809ed09
Horace Kuan <horacekuan@gmail.com>
2023-09-12 21:06:23 +1000

    Add tokens for typography

---
+++ src/constants/tokens.ts
@@ -0,0 +1,22 @@
+export const typography = {
+  headingLarge: {
+    fontSize: '64px',
+    lineHeight: '77px',
+  },
+  headingMedium: {
+    fontSize: '24px',
+    lineHeight: '29px',
+  },
+  headingSmall: {
+    fontSize: '20px',
+    lineHeight: '24px',
+  },
+  bodyMedium: {
+    fontSize: '18px',
+    lineHeight: '24px',
+  },
+  bodySmall: {
+    fontSize: '14px',
+    lineHeight: '17px',
+  },
+};
