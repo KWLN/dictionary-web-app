a407ed557afa699eb173aca037b8fad56abad4df
Horace Kuan <horacekuan@gmail.com>
2023-09-12 19:18:58 +1000

    Run ESLint and Prettier as pre-commit hook

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src="#" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
