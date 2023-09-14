829ae07dd0e56eb8251b9101ac7e818fb310942a
Horace Kuan <horacekuan@gmail.com>
2023-09-14 20:40:51 +1000

    Convert search input into a controlled component

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,10 +1,18 @@
+import { ChangeEvent, useState } from 'react';
 import { Icon, Input, InputWrapper } from './Search.styled';
 import SearchIcon from './assets/icon-search.svg';
 
 export function Search() {
+  const [inputValue, setInputValue] = useState<string>('');
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    setInputValue(event.target.value);
+  };
+
   return (
     <InputWrapper>
-      <Input placeholder="Search for any word..." />
+      <Input placeholder="Search for any word..." value={inputValue} onChange={handleInputChange} />
       <Icon src={SearchIcon} alt="" />
     </InputWrapper>
   );
