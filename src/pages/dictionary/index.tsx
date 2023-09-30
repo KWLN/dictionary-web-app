048cd6995129f883b4c68869978fccd2e1c492b8
Horace Kuan <horacekuan@gmail.com>
2023-09-30 17:13:31 +1000

    Add logo and page padding

--- src/pages/dictionary/index.tsx
+++ src/pages/dictionary/index.tsx
@@ -1,14 +1,21 @@
 import { DictionaryResult } from '../../components/dictionary-result/DictionaryResult';
 import { SearchBar } from '../../components/search-bar';
 import { useSearchDictionaryApi } from '../../services/dictionary-api';
+import { Page, PageContent, PageHeader } from './styled';
+import LogoSvg from '../../assets/images/logo.svg';
 
 export function DictionaryPage() {
   const { fetchDefinition, data, loading, error } = useSearchDictionaryApi();
 
   return (
-    <>
-      <SearchBar onSearch={fetchDefinition} />
-      <DictionaryResult resultData={data} isLoading={loading} error={error} />
-    </>
+    <Page>
+      <PageHeader>
+        <img src={LogoSvg} alt="" />
+      </PageHeader>
+      <PageContent>
+        <SearchBar onSearch={fetchDefinition} />
+        <DictionaryResult resultData={data} isLoading={loading} error={error} />
+      </PageContent>
+    </Page>
   );
 }
---
+++ src/pages/dictionary/styled.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Page = styled.div({
+  padding: '58px 352px',
+});
+
+export const PageHeader = styled.header({});
+
+export const PageContent = styled.div({});
