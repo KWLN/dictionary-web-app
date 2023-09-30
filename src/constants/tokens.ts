69b847ca9bb924aaca19afddd901f3896b22cec8
Horace Kuan <horacekuan@gmail.com>
2023-09-30 16:54:51 +1000

    Remove text-decoration from typography tokens

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -3,30 +3,25 @@ export const typography = {
     fontSize: '64px',
     fontWeight: 700,
     lineHeight: '77px',
-    textDecoration: 'none',
   },
   headingMedium: {
     fontSize: '24px',
     fontWeight: 400,
     lineHeight: '29px',
-    textDecoration: 'none',
   },
   headingSmall: {
     fontSize: '20px',
     fontWeight: 400,
     lineHeight: '24px',
-    textDecoration: 'none',
   },
   bodyMedium: {
     fontSize: '18px',
     fontWeight: 400,
     lineHeight: '24px',
-    textDecoration: 'none',
   },
   bodySmall: {
     fontSize: '14px',
     fontWeight: 400,
     lineHeight: '17px',
-    textDecoration: 'underline',
   },
 };
